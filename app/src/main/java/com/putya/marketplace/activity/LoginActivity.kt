4eca0b2c748d2e898474376a90e59235c2869e49
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2020-10-06 12:58:19 +0700

    activity_login.xml y

--- app/src/main/java/com/putya/marketplace/activity/LoginActivity.kt
+++ app/src/main/java/com/putya/marketplace/activity/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.putya.marketplace.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.putya.marketplace.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
